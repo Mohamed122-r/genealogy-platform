77c814d4c8736ce07f5a6e5d38c06ea88578ccda
Mohamed122-r <anskajamal@gmail.com>
2026-09-10 10:44:16 +0200

    Change matcher to restrict to admin routes

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,5 @@ import { authConfig } from "@/lib/auth.config";
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"],
+  matcher: ["/admin/:path*"],
 };
